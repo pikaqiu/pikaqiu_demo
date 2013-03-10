fe2a28f79c0c64387c6fae00bff6b8b3444f6f65
czzhf <zhf0729@gmail.com>
2013-03-10 16:11:42 +0800

    读取用户的输入

--- src/mathapp/main.go
+++ src/mathapp/main.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"mymath"
+	"os"
 )
 
 func main() {
-	fmt.Printf("Sqrt(2) = %v\n", mymath.Sqrt(2))
+	//fmt.Printf("Sqrt(2) = %v\n", mymath.Sqrt(2))
+
+	//当前所选取的机器人logic
+	currentLogic := "pikaqiu"
+
+	//循环读取用户的输入
+	running := true
+	reader := bufio.NewReader(os.Stdin)
+	for running {
+		data, _, _ := reader.ReadLine()
+		command := string(data)
+		switch command {
+		case "stop":
+			running = false
+		case "pikaqiu":
+			fmt.Println("currentLogic is ", currentLogic)
+		}
+		fmt.Println(command)
+	}
 }
